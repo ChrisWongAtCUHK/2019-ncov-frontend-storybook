2ee10fccfef318c2529f1f9d7b58097b8c3de67c
Chris Wong <wc38.wong@gmail.com>
2020-04-07 11:02:45 +0800

    Set components alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,20 @@
+const path = require('path');
+function resolve (dir) {
+    return path.join(__dirname, dir)
+}
+
 module.exports = {
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.resolve.alias['@components'] = path.join(__dirname, '../src/components')
+    console.log(JSON.stringify(config.resolve.alias, null, 2));
+
+    // Return the altered config
+    return config;
+  },
   stories: ['./stories/**/*.stories.[tj]s'],
 };
